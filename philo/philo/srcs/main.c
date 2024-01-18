7da9469e9bfe111ccbc872fe32dfb00e12529a58
Jeremy Tollenaers <jtollena@mi-r2-p3.s19.be>
2024-01-18 15:04:59 +0100

    save

--- philo/philo/srcs/main.c
+++ philo/philo/srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: jtollena <jtollena@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/11 14:25:29 by jtollena          #+#    #+#             */
-/*   Updated: 2024/01/18 14:57:42 by jtollena         ###   ########.fr       */
+/*   Updated: 2024/01/18 15:02:09 by jtollena         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,27 +31,39 @@ int	check_args(int argc, char **argv)
 }
 
 
-
-int	main(int argc, char **argv)
+int	init_game(int argc, char *argv[], t_game *game)
 {
-	t_game			game;
-	pthread_mutex_t	forks[200];
-	int				numphilos;
+	int	numphilos;
 
 	if (check_args(argc, argv) == 1)
 		return (1);
 	numphilos = ft_atoi(argv[1]);
 	if (numphilos > 200)
 		return (error("Too many Philosophers (max 200)."));
-	game.size = numphilos;
-	game.time_to_die = ft_atoi(argv[2]);
-	game.time_to_eat = ft_atoi(argv[3]);
-	game.time_to_sleep = ft_atoi(argv[4]);
+	game->size = numphilos;
+	game->time_to_die = ft_atoi(argv[2]);
+	game->time_to_eat = ft_atoi(argv[3]);
+	game->time_to_sleep = ft_atoi(argv[4]);
+	game->is_over = 0;
 	if (argc == 6)
-		game.eat_at_least = ft_atoi(argv[5]);
+		game->eat_at_least = ft_atoi(argv[5]);
 	else
-		game.eat_at_least = -1;
-	if (game.eat_at_least == 0)
-		return (printf("All Philosophers ate 0 times!\n"), 0);
-	return (threads_init(numphilos, &game));
+		game->eat_at_least = -1;
+	if (game->eat_at_least == 0)
+		return (printf("All Philosophers ate 0 times!\n"), 2);
+	return (0);
+}
+
+int	main(int argc, char **argv)
+{
+	t_game			game;
+	pthread_mutex_t	forks[200];
+	t_philo			philos[200];
+	int				init;
+
+	if ((init = init_game(argc, argv, &game)) == 1)
+		return (1);
+	if (init = 2)
+		return (0);
+	return (0);
 }
